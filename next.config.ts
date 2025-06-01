d1695aa4aa4d79119cf525ca35f5762b8136d238
mvinorian <vinorianmuhammad@gmail.com>
2025-06-01 17:21:52 +0700

    chore: set topLevelAwait to true

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    config.target = isServer ? 'node18' : ['web', 'es2022'];
+
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
